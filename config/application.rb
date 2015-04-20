cef0a115345c0e475edf904e8e863a324906ba54
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2015-04-20 16:59:11 -0500

    Init on precompile seems necessary?

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module RubyTheGryphon
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.initialize_on_precompile = true
   end
 end
