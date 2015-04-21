543d5b768f9a69456f7533cd2b991ba476b6e90b
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2015-04-21 15:35:36 -0500

    Ugh

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  config.action_mailer.delivery_method = :sendmail
 
   # config.assets.manifest = "public/assets/manifest-#{SecureRandom.hex(16)}.json"
 end
