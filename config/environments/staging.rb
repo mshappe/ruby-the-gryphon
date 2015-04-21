a851ca7ffcff7418d05b2114e7d2daaa0d3b0d56
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2015-04-21 15:23:46 -0500

    Fix sendmail

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -78,7 +78,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'rubygrphon.itasca.net' }
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
 
   # config.assets.manifest = "public/assets/manifest-#{SecureRandom.hex(16)}.json"
 end
