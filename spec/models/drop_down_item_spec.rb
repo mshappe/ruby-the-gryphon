741d99be4b7e9a89e5ddd45bd1e0f46ac24aa3c7
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2015-06-16 17:02:28 -0500

    Old DropDowns become new DropDownItems
    I had been converting each sub-list into separate tables, but that's not really
    necessary. STI works fine for this. Existing conversions will remain, and
    some things (States and Images) will just be done differently.

---
+++ spec/models/drop_down_item_spec.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: drop_down_items
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  type        :string
+#  active      :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe DropDownItem, :type => :model do
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_uniqueness_of :name }
+end
